fe7a56980c7bb91b800ca6b88dabece1a4008a40
Prince Vishwakarma <prince@counts.ac>
2023-11-20 12:48:47 +0530

    Add Tray

--- electron/main.ts
+++ electron/main.ts
@@ -1,6 +1,6 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu, Tray, Notification } from 'electron'
 import path from 'node:path'
-
+import { NotificationProps } from '../types'
 
 process.env.DIST = path.join(__dirname, '../dist')
 process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
@@ -12,12 +12,62 @@ const APP_ICON_ERROR = path.join(process.env.VITE_PUBLIC, 'icon-counts-error.png
 const APP_ICON_WARNING = path.join(process.env.VITE_PUBLIC, 'icon-counts-warning.png')
 
 
-let win: BrowserWindow | null
+let WIN: BrowserWindow;
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+let tray: Tray;
+let QUIT = true;
+
+function showNotification({ title, body }: NotificationProps) {
+
+  const notification = new Notification({ title, body, icon: APP_ICON, });
+  notification.show();
+  notification.on('click', () => {
+    // console.log(CLICK_MESSAGE)
+  })
+
+}
+
+function createTray() {
+  tray = new Tray(APP_ICON);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open', click: () => {
+        if (!WIN) {
+          createWindow();
+        }
+        WIN?.show();
+        WIN?.setAlwaysOnTop(true);
+      },
+    },
+    { type: 'separator' },
+    {
+      label: 'Quit', click: () => {
+        QUIT = false;
+        app.quit()
+      }
+    }
+  ]);
+
+  tray.setToolTip(APP_NAME);
+  tray.setTitle(APP_NAME)
+  tray.setContextMenu(contextMenu);
+
+
+  tray.on('click', () => {
+    if (!WIN) {
+      createWindow();
+    }
+    WIN?.show();
+    WIN?.setAlwaysOnTop(true);
+  });
+
+}
+
 
 function createWindow() {
-  win = new BrowserWindow({
+  WIN = new BrowserWindow({
     icon: APP_ICON,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
@@ -26,16 +76,27 @@ function createWindow() {
   })
 
   // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
+  WIN.webContents.on('did-finish-load', () => {
+    WIN?.webContents.send('main-process-message', (new Date).toLocaleString())
   })
 
   if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
+    WIN.loadURL(VITE_DEV_SERVER_URL)
   } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(process.env.DIST, 'index.html'))
+    // WIN.loadFile('dist/index.html')
+    WIN.loadFile(path.join(process.env.DIST, 'index.html'))
   }
+
+  WIN.on('close', (event) => {
+    if (QUIT) {
+      event.preventDefault();
+      showNotification({ title: "Counts Sync", body: "App running in background" })
+    }
+    WIN?.hide();
+  });
+
+  WIN.once('focus', () => WIN.flashFrame(false))
+  WIN.flashFrame(true)
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -44,7 +105,6 @@ function createWindow() {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
-    win = null
   }
 })
 
@@ -56,4 +116,7 @@ app.on('activate', () => {
   }
 })
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow();
+  createTray();
+})
